b3e05a1621a12396aa46eb1502c200947b341c8d
gilles.meyhi <gilles.meyhi@student.kuleuven.be>
2019-12-25 19:07:27 +0100

    First working version

--- aStar.cpp
+++ aStar.cpp
@@ -1,104 +1,159 @@
-#include <queue>
-#include <unordered_map>
-#include "Node.h"
-#include "modelworld.h"
 
-#define currPos ((currentNode.y)-1)*w->getCols() + currentNode.x
-#define NORTH currPos //HIER BEN IK GEEINDIGD!!!
 
-class Compare
+#include "aStar.h"
+
+
+inline std::pair<int,int> operator + (const std::pair<int,int>& lhs , const std::pair<int,int> &rhs )
 {
-public:
-    bool operator() (Node n1 , Node n2)
-    {
-        return n1.finalCost < n2.finalCost ;
-    }
-};
-
-bool operator == (Node &n1, Node &n2){return n1.x == n2.x && n1.y == n2.y;}
-bool operator != (Node &n1, Node &n2){return n1.x != n2.x || n1.y != n2.y;}
-
-
-
-class aStar  {
-public :
-
-    aStar(ModelWorld * w);
-    ModelWorld *w;
-
-
-    //!!!!!!!!!!!!!!!!!!!!!! CONSIDER USING UNIQUE POINTERS TO NODES INSTEAD OF THE NODES ITSELF !!!!!!!!!!!!!!
-    std::priority_queue <Node,std::vector<Node>,Compare> openlist;
-    //std::unordered_map <Node> closedlist;  For later
-    std::vector <Node> closedlist; // inneficient for now
-
-    std::vector <Node> solution;
-
-    std::vector<Node> runAlgo(int startx, int starty, int endx, int endy);
-    double calcHeuristic (int x , int y , int xend , int yend);
-
-};
-
-aStar::aStar(ModelWorld *w)
-{
-    this->w = w;
+    std::pair<int,int> result;
+    result.first = lhs.first+rhs.first;
+    result.second = lhs.second+rhs.second;
+    return result;
 }
 
-std::vector<Node> aStar::runAlgo(int startx, int starty, int endx, int endy)
+struct find_by_pos {
+    find_by_pos(const std::pair<int,int> & pos) : pos(pos) {}
+    bool operator()(const Node * node) {
+        return (*node).pos == pos;
+    }
+private:
+    std::pair<int,int> pos;
+};
+
+
+
+
+bool operator == (Node &n1, Node &n2){return n1.pos == n2.pos;}
+bool operator != (Node &n1, Node &n2){return n1.pos != n2.pos;}
+
+
+aStar::aStar(std::vector<std::vector<std::shared_ptr<MyTile>>> &tiles)
 {
-    solution.clear();
+    map = tiles;
+}
 
-    // Create root node
-    Node root = Node(startx,starty);
-    root.partentPointer = nullptr;
-    root.finalCost = 0 + calcHeuristic(root.x , root.y , endx , endy);
+std::vector<Node> aStar::runAlgo(Node* player, Node* dest)
+{
 
-    // Push root into openlist
-    openlist.push(root);
 
-    // main loop
-    while(!openlist.empty())
-    {
-        // pop the node with the best finalCost from open and assign it to currentNode
-        Node currentNode = openlist.top();
-        openlist.pop();
+    std::vector<std::pair<int,int>> dirs;
+    dirs.emplace_back(std::make_pair(1,0));
+    dirs.emplace_back(std::make_pair(-1,0));
+    dirs.emplace_back(std::make_pair(0,1));
+    dirs.emplace_back(std::make_pair(0,-1));
 
-        //if currentNode's x and y are equal to the goalPoint, then
-        if(currentNode.x == endx && currentNode.y == endy)
-        {
-            /* Note: we can traverse through the parents of currentNode
-            - push the nodes that are part of the path in to solution*/
-            while (currentNode != root)
-            {
-                solution.push_back(currentNode);
-                currentNode = *currentNode.partentPointer;
-            }
-            //- break from step 3
-            return solution;
+    std::vector<Node> empty;
+        if (isValid((*dest).pos.first, (*dest).pos.second) == false) {
+            std::cout << "Destination is an obstacle" << std::endl;
+            return empty;
+            //Destination is invalid
+        }
+        if (isDestination((*player).pos.first, (*player).pos.second, (*dest))) {
+            std::cout << "You are the destination" << std::endl;
+            return empty;
+            //You clicked on yourself
         }
 
 
-        // Note: a node has 8 points around it which can be used to create min of 0 and
-        //       max of 8 successor
-        //for every nearbyPoint around the currentNode do the following
+        player->fCost = 0;
+        player->parent = nullptr;
 
-        //NORTH
-        if(w->getTiles().at())
-        /*1)if this nearbyPoint is in a spot that is illegal such as a wall, then
-            - skip to the next nearbyPoint*/
+        openList.insert(player);
 
-    }
+        while(!openList.empty())
+        {
+            auto itNode = openList.begin();
+            Node * currentNode = *itNode;
+            openList.erase(itNode);
 
-    throw "Destination unreachable";
 
+            if((*currentNode).pos==(*dest).pos) // The dest has been found
+            {
+                while((*currentNode).parent!=nullptr)// Push the solution
+                {
+                    (*currentNode) = *(*currentNode).parent;
+                    solution.push_back(*currentNode);
+
+                }
+                return solution;
+
+            }
+
+
+
+            for(auto it = std::begin(dirs); it != std::end(dirs); it++) // For each neighbor
+            {
+                if(isValid((*currentNode).pos.first+it->first,(*currentNode).pos.second+it->second))
+                {
+                    Node * successorNode = new Node();
+                    (*successorNode).pos = (*currentNode).pos + *it;
+                    (*successorNode).parent =  currentNode;
+                    (*successorNode).gCost = (*currentNode).gCost + map.at((*successorNode).pos.first).at((*successorNode).pos.second)->getValue();
+                    (*successorNode).hCost = calculateH((*successorNode).pos.first,(*successorNode).pos.second,(*dest));
+                    (*successorNode).fCost = (*successorNode).gCost+(*successorNode).hCost;
+
+
+                    std::pair<int,int> neigborPos = (*currentNode).pos + *it;
+                    float ngCost = (*currentNode).gCost + map.at(neigborPos.first).at(neigborPos.second)->getValue();
+
+                   auto result = std::find_if(openList.begin(), openList.end(),
+                                                                  find_by_pos(neigborPos)); //checks if neigbor in openlist
+                    if(result != openList.end()) {
+                        // we found something now check if better
+
+                        if((*result)->gCost > ngCost) // if neighbor is a better node
+                        {
+                            openList.erase(result);
+                        }
+                        else{continue;}
+
+                    }
+                    auto smth = std::find_if(closedlist.begin(), closedlist.end(), find_by_pos(neigborPos));
+                    // Check if in closedlist
+                    if(smth != closedlist.end())
+                    {
+                        if((*smth)->gCost > ngCost)
+                        {// if neighbor is a better node
+                            /* v contains x */
+                            closedlist.erase(smth);
+                        }
+                        else{continue;}
+                    }
+
+                    openList.insert(successorNode);
+
+                }
+
+            }
+            closedlist.emplace_back(currentNode);
+
+
+
+
+        }
+        std::cout<<"Goal is unreachable"<<std::endl;
+    return empty;
 }
 
 
+inline double aStar::calculateH(int x, int y, Node dest) {
+    double H = (sqrt((x - dest.pos.first)*(x - dest.pos.first)
+        + (y - dest.pos.second)*(y - dest.pos.second)));
+    return H;
+}
 
-double aStar::calcHeuristic(int x, int y, int xend, int yend)
+
+bool aStar::isValid(int x , int y)
 {
-    return( (double) sqrt((x-xend)*(x-xend)+(y-yend)*(y-yend)));
+    if(x >= 0 && x <= getRows() && y >= 0 && y <= getCols() && map.at(x).at(y)->getValue()!=1.0f){return true;}
+    else {return false;}
 }
 
 
+bool aStar::isDestination(int x, int y, Node dest) {
+    if (x == dest.pos.first && y == dest.pos.second) {
+        return true;
+    }
+    return false;
+}
 
---
+++ aStar.h
@@ -0,0 +1,55 @@
+#ifndef ASTAR_H
+#define ASTAR_H
+#include <set>
+#include <unordered_map>
+#include "modelworld.h"
+#include <algorithm>
+#include "world.h"
+#include <math.h>
+struct Node
+{
+    std::pair<int,int> pos;
+    const Node * parent;
+    float hCost;
+    float gCost;
+    float fCost;
+    Node(int x,int y)
+    {
+        pos.first = x;
+        pos.second = y;
+    }
+    Node(){}
+};
+
+struct APtrComp
+{
+  bool operator()(const Node* lhs, const Node* rhs) const  { return (lhs)->fCost < (rhs)->fCost; }
+};
+
+
+
+
+class aStar : public World{
+public :
+
+    aStar(std::vector<std::vector<std::shared_ptr<MyTile>>> &tiles);
+
+    std::vector<std::vector<std::shared_ptr<MyTile>>> map;
+
+    //!!!!!!!!!!!!!!!!!!!!!! CONSIDER USING UNIQUE POINTERS TO NODES INSTEAD OF THE NODES ITSELF !!!!!!!!!!!!!!
+    //std::priority_queue <Node,std::vector<Node>,Compare> openlist;
+    //std::unordered_map <Node> closedlist;  For later
+    std::vector <Node*> closedlist; // inneficient for now
+    std::set<Node*,APtrComp> openList;
+
+    std::vector <Node> solution;
+
+    std::vector<Node> runAlgo(Node* player, Node* dest);
+    double calculateH(int x, int y, Node dest);
+    bool isValid(int x , int y);
+    bool isDestination(int x, int y, Node dest);
+
+
+
+};
+#endif // ASTAR_H
